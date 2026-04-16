b68a107b719eb9e535381a5cadc9dcdf037ac43d
Liam Nagels <Liam.Nagels@outlook.be>
2026-04-16 13:46:02 +0200

    reformat and small performance upgrade using cashing

--- Assets/Scripts/NeuralNetwork/Layers/HiddenLayer.cs
+++ Assets/Scripts/NeuralNetwork/Layers/HiddenLayer.cs
@@ -2,35 +2,40 @@ using UnityEngine;
 [System.Serializable]
 public class HiddenLayer : Layer
 {
-    [SerializeField] private HiddenNeuron[] HiddenNeurons;
+	[SerializeField] private HiddenNeuron[] HiddenNeurons;
+	private float[] Values;
 
-    public HiddenNeuron[] GetNeurons()
-    {
-        return HiddenNeurons;
-    }
+	public HiddenNeuron[] GetNeurons()
+	{
+		return HiddenNeurons;
+	}
 
-    public void InitializeLayer(int size, float[] previousLayerValues)
-    {
-        HiddenNeurons = new HiddenNeuron[size];
-        for (int i = 0; i < size; i++)
-        {
-            HiddenNeurons[i] = new HiddenNeuron(); 
-        }
+	public void InitializeLayer(int size, float[] previousLayerValues)
+	{
+		HiddenNeurons = new HiddenNeuron[size];
+		for (int i = 0; i < size; i++)
+		{
+			HiddenNeurons[i] = new HiddenNeuron();
+		}
 
-        foreach (HiddenNeuron neuron in HiddenNeurons)
-        {
-            neuron.SetPreviousLayerValues(previousLayerValues);
-            neuron.InitializeNeuron();
-        }
-    }
+		foreach (HiddenNeuron neuron in HiddenNeurons)
+		{
+			neuron.SetPreviousLayerValues(previousLayerValues);
+			neuron.InitializeNeuron();
+		}
+	}
 
-    public float[] GetAllNeuronValues()
-    {
-        float[] values = new float[HiddenNeurons.Length];
-        for (int i = 0; i < HiddenNeurons.Length; i++)
-        {
-            values[i] = HiddenNeurons[i].GetNeuronValue();
-        }
-        return values;
-    }
+	public float[] GetAllNeuronValues()
+	{
+		if (Values == null)
+		{
+			Values = new float[HiddenNeurons.Length];
+		}
+
+		for (int i = 0; i < HiddenNeurons.Length; i++)
+		{
+			Values[i] = HiddenNeurons[i].GetNeuronValue();
+		}
+		return Values;
+	}
 }
